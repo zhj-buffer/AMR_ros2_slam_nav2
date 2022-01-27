d3840aff4f918d879b691b411988edc90c1d6719
alan Zhang <zhj.buffer@gmail.com>
2022-01-27 13:38:50 +0800

    update navigation2 launcher
    Signed-off-by: alan Zhang <zhj.buffer@gmail.com>

--- amr_navigation2/launch/navigation2.launch.py
+++ amr_navigation2/launch/navigation2.launch.py
@@ -24,7 +24,7 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
-TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
+#TURTLEBOT3_MODEL = os.environ['TURTLEBOT3_MODEL']
 
 
 def generate_launch_description():
